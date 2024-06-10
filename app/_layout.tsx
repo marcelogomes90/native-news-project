cab5d7c33939a7d296e46c52ade242ce706a2ee6
Marcelo <marcelo.sobrinho@outlook.com>
2024-06-10 16:31:05 -0300

    chore: add react query and axios

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -8,17 +8,49 @@ import { Stack } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
 import { useEffect } from "react";
 import "react-native-reanimated";
+import {
+  onlineManager,
+  focusManager,
+  QueryCache,
+  QueryClientProvider,
+  QueryClient,
+} from "@tanstack/react-query";
+import NetInfo from "@react-native-community/netinfo";
+import { AppState } from "react-native";
+import type { AppStateStatus } from "react-native";
 
 import { useColorScheme } from "@/hooks/useColorScheme";
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
+const queryClient = new QueryClient({
+  queryCache: new QueryCache(),
+});
+
 export default function RootLayout() {
   const colorScheme = useColorScheme();
   const [loaded] = useFonts({
     SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
   });
+
+  useEffect(() => {
+    onlineManager.setEventListener((setOnline) => {
+      return NetInfo.addEventListener((state) => {
+        setOnline(!!state.isConnected);
+      });
+    });
+  }, []);
+
+  useEffect(() => {
+    const subscriber = AppState.addEventListener("change", onFocusRefetch);
+
+    return () => subscriber.remove();
+  }, []);
+
+  const onFocusRefetch = (status: AppStateStatus) => {
+    focusManager.setFocused(status === "active");
+  };
 
   useEffect(() => {
     if (loaded) {
@@ -31,11 +63,13 @@ export default function RootLayout() {
   }
 
   return (
-    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+        <Stack>
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          <Stack.Screen name="+not-found" />
+        </Stack>
+      </ThemeProvider>
+    </QueryClientProvider>
   );
 }
