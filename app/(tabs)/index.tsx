4a33b4d2db856e88742f153545f1e23942524a6b
Marcelo <marcelo.sobrinho@outlook.com>
2024-06-11 10:15:22 -0300

    feat: add news list

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,41 @@
-import { Image, StyleSheet } from "react-native";
-
+import { FlatList, Image, StyleSheet } from "react-native";
 import ParallaxScrollView from "@/components/ParallaxScrollView";
 import { ThemedText } from "@/components/ThemedText";
 import { ThemedView } from "@/components/ThemedView";
+import { useQuery } from "@tanstack/react-query";
+import { fetchNews } from "@/processes/news";
+import { useCallback } from "react";
+import { NewsData } from "@/interfaces/news";
+import noImage from "@/assets/images/noimage.png";
 
 export default function HomeScreen() {
+  const {
+    data: newsList,
+    error,
+    isLoading,
+  } = useQuery({
+    queryFn: fetchNews,
+    queryKey: ["news", { qtd: 5 }],
+  });
+
+  const renderImage = useCallback((item: { imagens: string }) => {
+    const image = item?.imagens
+      ? `https://agenciadenoticias.ibge.gov.br/${JSON.parse(item?.imagens)?.image_intro}`
+      : noImage;
+
+    return image;
+  }, []);
+
+  const renderItem = useCallback(
+    (item: NewsData) => (
+      <ThemedView style={styles.newsContainer}>
+        <Image source={{ uri: renderImage(item) }} style={styles.newsImage} />
+        <ThemedText style={styles.newsTitle}>{item.titulo}</ThemedText>
+      </ThemedView>
+    ),
+    [renderImage],
+  );
+
   return (
     <ParallaxScrollView
       headerBackgroundColor={{ light: "#A1CEDC", dark: "#1D3D47" }}
@@ -18,6 +49,14 @@ export default function HomeScreen() {
       <ThemedView style={styles.titleContainer}>
         <ThemedText type="title">Notícias IBGE</ThemedText>
       </ThemedView>
+
+      <FlatList
+        data={newsList?.items}
+        renderItem={({ item }) => renderItem(item)}
+        keyExtractor={(item) => item.id.toString()}
+        showsHorizontalScrollIndicator={false}
+        showsVerticalScrollIndicator={false}
+      />
     </ParallaxScrollView>
   );
 }
@@ -27,6 +66,8 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     alignItems: "center",
     gap: 8,
+    marginBottom: 16,
+    justifyContent: "center",
   },
   reactLogo: {
     height: 178,
@@ -34,5 +75,22 @@ const styles = StyleSheet.create({
     bottom: 0,
     left: 0,
     position: "absolute",
+  },
+  newsContainer: {
+    display: "flex",
+    flexDirection: "row",
+    gap: 16,
+    marginBottom: 24,
+    alignItems: "center",
+  },
+  newsTitle: {
+    fontSize: 18,
+    fontWeight: "bold",
+    flexShrink: 1,
+  },
+  newsImage: {
+    width: 120,
+    height: 120,
+    borderRadius: 8,
   },
 });
---
+++ processes/news.ts
@@ -0,0 +1,13 @@
+import { NewsResponse } from "../interfaces/news";
+import axios from "axios";
+
+export const fetchNews = async ({ queryKey }: { queryKey: any[] }) => {
+  const [, params] = queryKey;
+
+  const { data } = await axios.get<NewsResponse>(
+    "https://servicodados.ibge.gov.br/api/v3/noticias/",
+    { params },
+  );
+
+  return data;
+};
